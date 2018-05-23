42a2a6e703055546c180915e8d49b102d431ad8b
Alan Curtis <fakeemail.@email.com>
2018-05-22 23:40:57 -0600

    fixed bug where drawing two of same suit would only update one.

--- src/App.js
+++ src/App.js
@@ -29,42 +29,56 @@ class App extends Component {
     axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/?count=2`).then(res=>{
       // Destructure this.state 
       let {clubs,  spades,  hearts, diamonds, queensFound, cardsDrawn} = this.state
+      let newCardsDiamonds=[]
+      let newCardsHearts=[]
+      let newCardsSpades=[]
+      let newCardsClubs=[]
       for (let i in res.data.cards){
-        console.log(res.data.cards)
         // for each card drawn check the suit and if its a queen, sort into corresponding list and increment queensFound.
         switch (res.data.cards[i].suit) {
           case "CLUBS": 
-          if(res.data.cards[i].value === "QUEEN"){
-            this.setState({clubs: [...clubs, res.data.cards[i]], queensFound: queensFound +=1, cardsDrawn: cardsDrawn + 2 })
-          }else{
-            this.setState({clubs: [...clubs, res.data.cards[i]], cardsDrawn: cardsDrawn + 2})
-          }
+            if(res.data.cards[i].value === "QUEEN"){
+              this.setState({ queensFound: queensFound +=1 })
+              newCardsClubs.push(res.data.cards[i])
+            }else{
+              newCardsClubs.push(res.data.cards[i])
+            }
             break;
           case "DIAMONDS": 
-          if(res.data.cards[i].value === "QUEEN"){
-            this.setState({diamonds: [...diamonds, res.data.cards[i]], queensFound: queensFound +=1 , cardsDrawn: cardsDrawn + 2})
-          }else{
-            this.setState({diamonds: [...diamonds, res.data.cards[i]], cardsDrawn: cardsDrawn + 2})
-          }
-            break;
+            if(res.data.cards[i].value === "QUEEN"){
+              this.setState({ queensFound: queensFound +=1 })
+              newCardsDiamonds.push(res.data.cards[i])
+            }else{
+              newCardsDiamonds.push(res.data.cards[i])
+            }
+          break;
           case "SPADES": 
-          if(res.data.cards[i].value === "QUEEN"){
-            this.setState({spades: [...spades, res.data.cards[i]], queensFound: queensFound +=1 , cardsDrawn: cardsDrawn + 2})
-          }else{
-            this.setState({spades: [...spades, res.data.cards[i]], cardsDrawn: cardsDrawn + 2})
-          }
-            break;
+            if(res.data.cards[i].value === "QUEEN"){
+              this.setState({queensFound: queensFound +=1 })
+              newCardsSpades.push(res.data.cards[i])
+            }else{
+              newCardsSpades.push(res.data.cards[i])
+            }
+          break;
           case "HEARTS":
-          if(res.data.cards[i].value === "QUEEN"){
-            this.setState({hearts: [...hearts, res.data.cards[i]], queensFound: queensFound +=1,cardsDrawn: cardsDrawn + 2 })
-          }else{
-            this.setState({hearts: [...hearts, res.data.cards[i]], cardsDrawn: cardsDrawn + 2})
-          }
+            if(res.data.cards[i].value === "QUEEN"){
+              this.setState({ queensFound: queensFound +=1 })
+              newCardsHearts.push(res.data.cards[i])
+            }else{
+              newCardsHearts.push(res.data.cards[i])
+            }
             break;
           default: console.log('defaulted')
             break;
         }
       }
+      this.setState({
+        hearts:[...hearts, ...newCardsHearts],
+        clubs:[...clubs, ...newCardsClubs],
+        diamonds:[...diamonds, ...newCardsDiamonds],
+        spades:[...spades, ...newCardsSpades],
+        cardsDrawn: cardsDrawn + 2
+      })
     })
   }
 
@@ -78,7 +92,7 @@ class App extends Component {
       }else{
         console.log('Done')
       }
-    }, 1500);
+    }, 1000);
   }
 
   reset(){
@@ -139,7 +153,7 @@ class App extends Component {
               <button onClick={()=>this.start()}>Start</button>
               <button onClick={()=>this.reset()}>Reset</button>
             </div>
-            <button onClick={()=>this.drawTwo()}>drawtwo</button>
+            {/* <button onClick={()=>this.drawTwo()}>drawtwo</button> */}
             <img className='deck' src='/images/CardBack.png' alt='deck'/>
             <h3>Cards Drawn:{this.state.cardsDrawn}</h3>
           </div>
